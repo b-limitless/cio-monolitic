505de61f99f33bb5b2a6ed3f37e1590ff7f74cb6
Bharat Shah <akashche72@gmail.com>
2024-01-18 18:57:56 +0400

    adding style to the model

---
+++ src/models/style.ts
@@ -0,0 +1,61 @@
+import mongoose from "mongoose";
+
+export enum ProductType {
+  shirt = "shirt",
+  pant = "pant",
+  suit = "suit",
+}
+
+export enum ProductPartNames {
+  collar = "collar",
+  cuff = "cuff",
+}
+
+export interface StyleAttrs {
+  type: ProductType;
+  partName: ProductPartNames;
+  price: number;
+  title: string;
+  code: string;
+  label: string;
+  model: string;
+}
+interface StyleDoc extends mongoose.Document {
+  type: ProductType;
+  partName: ProductPartNames;
+  price: number;
+  title: string;
+  code: string;
+  label: string;
+  model: string;
+}
+interface StyleModel extends mongoose.Model<StyleDoc> {
+  build(attrs: StyleAttrs): StyleDoc;
+}
+const StyleSchema = new mongoose.Schema(
+  {
+    type: { type: String, enum: Object.values(ProductType) },
+    partName: { type: String, enum: Object.values(ProductPartNames) },
+    price: { type: Number, required: true },
+    title: { type: String, required: true },
+    code: { type: String, required: true },
+    label: { type: String, required: true },
+    model: { type: String, required: true },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+StyleSchema.statics.build = (attrs: StyleAttrs) => {
+  return new Style(attrs);
+};
+const Style = mongoose.model<StyleDoc, StyleModel>("Style", StyleSchema);
+export { Style };
