0b2dd6511dfa3c31bc990c7b3fbde14d547b320b
Bharat Shah <akashche72@gmail.com>
2024-02-01 16:19:24 +0400

    adding patch method to cors

--- src/app.ts
+++ src/app.ts
@@ -46,7 +46,7 @@ app.use(
   cors({
     credentials: true,
     origin: 'http://localhost:3000', 
-    methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
+    methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD', 'PATCH'],
   })
 );
 
